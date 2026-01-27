e69f410b5b05053bd183dd6647d93c85538361c0
MH <mihug2004@gmail.com>
2026-01-27 15:50:18 +0700

    feat(basket): implemenet Get, Store, and Delete basket features using Vertical Slide Architecture

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,16 @@
+namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart ShoppingCart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        // TODO: get basket from database
+        // var basket = await _repository.GetBasket(request.UserName);
+
+        return new GetBasketResult(new ShoppingCart("swn"));
+    }
+}
