e42cb3edb8e67d908369c475de9188e967d3137b
MH <mihug2004@gmail.com>
2026-02-05 21:28:05 +0700

    feat(ordering): implement Minimal API endpoints using Carter

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -0,0 +1,33 @@
+using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+namespace Ordering.API.Endpoints;
+
+// accepts a customer ID.
+// uses a GetOrderByCustomerQuery to fetch orders.
+// returns the list of orders for that customer.
+
+// public record GetOrdersByCustomerRequest(Guid CustomerId);
+
+public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+
+public class GetOrdersByCustomer : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersByCustomerQuery(customerId));
+
+                var response = result.Adapt<GetOrdersByCustomerResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetOrdersByCustomer")
+            .Produces<GetOrdersByCustomerResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Orders By Customer")
+            .WithDescription("Get Orders By Customer");
+        ;
+    }
+}
