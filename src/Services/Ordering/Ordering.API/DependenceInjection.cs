2148a18cc8a3b21eb8345a7e412b3e31f03ced14
MH <mihug2004@gmail.com>
2026-02-05 21:28:48 +0700

    chore(ordering): configure dependency injection and middleware pipeline for API

--- src/Services/Ordering/Ordering.API/DependenceInjection.cs
+++ src/Services/Ordering/Ordering.API/DependenceInjection.cs
@@ -1,16 +1,35 @@
+using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 namespace Ordering.API;
 
 public static class DependenceInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
-        // services.AddCarter();
+        services.AddCarter();
+
+        services.AddExceptionHandler<CustomExceptionHandler>();
+
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
+
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
-        // app.MapCarter();
+        app.MapCarter();
+
+        app.UseExceptionHandler(options => { });
+
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
         return app;
     }
 }
